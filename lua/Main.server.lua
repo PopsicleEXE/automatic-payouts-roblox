46e4c50d6f39947e9de995d9a43824e989234ccd
PopsicleEXE <65886693+PopsicleEXE@users.noreply.github.com>
2021-05-02 13:25:03 -0500

    Remove examples for removed API calls

--- lua/Main.server.lua
+++ lua/Main.server.lua
@@ -7,16 +7,5 @@ local Server = require(script.Server)
 -- EXAMPLE USAGE
 
 --[[
-	
-	Server.Promote(GroupIdHere, PlayerUserIdHere)
-	Server.Demote(GroupIdHere, PlayerUserIdHere)
-	Server.SetRank(GroupIdHere, PlayerUserIdHere, RankIdHere)
 	Server.Payout(GroupIdHere, PlayerUserIdHere, RobuxAmountHere)
-	
-	Server.HandleJoinRequest(GroupIdHere, "PlayerUsernameHere", true) -- Accept the request
-	Server.HandleJoinRequest(GroupIdHere, "PlayerUsernameHere", false) -- Decline the request
-
-	Server.GroupShout(GroupIdHere, "MessageToShoutHere")
-	Server.SetRank(GroupIdHere, PlayerUserIdHere, RankIdHere)
-	
 ]]
